a8b1ed127c25829b8cf4673d2e9259c6b7e28db4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 09:52:29 +0530

    rs-vpc

--- gateways.tf
+++ gateways.tf
@@ -1,7 +1,7 @@
 resource "aws_internet_gateway" "igw" {
   vpc_id            = aws_vpc.vpc.id
     tags              = {
-      name          = "${var.project}-${var.env}-eip"
+      name          = "${var.project}-${var.env}-igw"
       environemt    = var.env
       provisioned   = "terraform"
   }
@@ -19,5 +19,10 @@ resource "aws_eip" "eip" {
 resource "aws_nat_gateway" "ngw" {
     allocation_id = aws_eip.eip
     subnet_id     = aws_subnet.public-subnets.*.[0]
-    
+    tags              = {
+      name          = "${var.project}-${var.env}-ngw"
+      environemt    = var.env
+      provisioned   = "terraform"
+  }
+
 }
