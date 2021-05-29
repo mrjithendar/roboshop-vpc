1a592428835456f9ebe2af7813de9b27c0f83a17
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 09:33:29 +0530

    rs-vpc

--- vpc.tf
+++ vpc.tf
@@ -11,21 +11,9 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "private-subnets" {
     count                  = 2
-    #count is a meta-argument defined by the Terraform language. It can be used with modules and with every resource type. The count meta-argument accepts a whole number, and creates that many instances of the resource or module.
     cidr_block             = cidrsubnet(var.vpc_cidr,2, count.index)
-    #cidrsubnet Function:
-    #cidrsubnet calculates a subnet address within given IP network address prefix. Ex= cidrsubnet(prefix, newbits, netnum).
-
-    #prefix must be given in CIDR notation, as defined.
-
-    #newbits is the number of additional bits with which to extend the prefix. For example, if given a prefix ending in /16 and a newbits value of 4, the resulting subnet address will have length /20.
-
-    #netnum is a whole number that can be represented as a binary integer with no more than newbits binary digits, which will be used to populate the additional bits added to the prefix.
-
-    #element Function: element retrieves a single element from a list. The index is zero-based. This function produces an error if used with an empty list.
     vpc_id                 = aws_vpc.vpc.id
     availability_zone      = element(data.aws_availablity_zones.avz.names, count.index)
-    ##element Function: element retrieves a single element from a list. The index is zero-based. This function produces an error if used with an empty list.
     tags                   = {
         name               = "${var.project}-${var.env}-private-subnet-${count.index+1}"
         environment        = var.env
@@ -36,7 +24,6 @@ resource "aws_subnet" "private-subnets" {
 resource "aws_subnet" "public-subnets" {
     count                  = 2
     cidr_block             = cidrsubnet(var.vpc_cidr,2, count.index)
-    #When count is set, Terraform distinguishes between the block itself and the multiple resource or module instances associated with it. Instances are identified by an index number, starting with 0.
     vpc_id                 = aws_vpc.vpc.id
     availability_zone      = element(data.aws_availablity_zones.name, count.index+1)
     tags                   = {
@@ -45,3 +32,21 @@ resource "aws_subnet" "public-subnets" {
         provisioned        = "terraform"
     } 
 }
+
+
+    #count is a meta-argument defined by the Terraform language. It can be used with modules and with every resource type. The count meta-argument accepts a whole number, and creates that many instances of the resource or module.
+
+    #cidrsubnet Function:
+    #cidrsubnet calculates a subnet address within given IP network address prefix. Ex= cidrsubnet(prefix, newbits, netnum).
+
+    #prefix must be given in CIDR notation, as defined.
+
+    #newbits is the number of additional bits with which to extend the prefix. For example, if given a prefix ending in /16 and a newbits value of 4, the resulting subnet address will have length /20.
+
+    #netnum is a whole number that can be represented as a binary integer with no more than newbits binary digits, which will be used to populate the additional bits added to the prefix.
+
+    #element Function: element retrieves a single element from a list. The index is zero-based. This function produces an error if used with an empty list.
+
+    #When count is set, Terraform distinguishes between the block itself and the multiple resource or module instances associated with it. Instances are identified by an index number, starting with 0.
+
+    ##element Function: element retrieves a single element from a list. The index is zero-based. This function produces an error if used with an empty list.
