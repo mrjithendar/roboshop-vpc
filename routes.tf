a3d0fd3372ba399beb0e0929501370fea2928cf4
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 11:11:55 +0530

    rs-vpc

--- routes.tf
+++ routes.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "private" {
     }
     route {
         cidr_block                = var.default_vpc_cidr
-        vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+        vpc_peering_connection_id = aws_vpc_peering_connection.vpc-peering.id
     }
     tags {
         name        = "${var.project}-${var.env}-pri-rtable"
