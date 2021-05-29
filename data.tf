bf27133cde8dd90214ac92561eea04fee2c2244c
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 08:51:40 +0530

    rs-vpc

---
+++ data.tf
@@ -0,0 +1,14 @@
+data "aws_route53_zone" "jithendar" {
+  name = "jithendar.com"
+  private_zone = false
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-rs-practice"
+    key    = "rs-instances/cart.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "aws_availability_zones" "avz" {}
